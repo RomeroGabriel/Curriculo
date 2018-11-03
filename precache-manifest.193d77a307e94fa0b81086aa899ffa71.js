3efcc222c53ebe9bfebbdfcd16cb00b4e25502cf
Gabriel Romero <gabrielromerods@hotmail.com>
2018-11-03 14:27:28 -0300

    Updates

---
+++ precache-manifest.193d77a307e94fa0b81086aa899ffa71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3ee2dd71bb4fade9300",
+    "url": "/Curriculo/static/js/runtime~main.a3ee2dd7.js"
+  },
+  {
+    "revision": "20764d8f7934e5097fa0",
+    "url": "/Curriculo/static/js/main.20764d8f.chunk.js"
+  },
+  {
+    "revision": "4b009694ac8815be9cdb",
+    "url": "/Curriculo/static/js/1.4b009694.chunk.js"
+  },
+  {
+    "revision": "20764d8f7934e5097fa0",
+    "url": "/Curriculo/static/css/main.e549b771.chunk.css"
+  },
+  {
+    "revision": "b9a93380456897749bfa8d985aba7a6d",
+    "url": "/Curriculo/index.html"
+  }
+];
